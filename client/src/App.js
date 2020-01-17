c8938927c267cca2c3687e979a72f6a71737966c
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2020-01-17 10:06:40 -0600

    getting things setup. refactored app as class component

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// components
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      players: []
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Women's World Cup Soccer Players</h1>
+      </>
+    )
+  }
 }
 
 export default App;
